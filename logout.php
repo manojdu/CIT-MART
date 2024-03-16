ae1f81d061adea359d8630c392b9b3f8bd58a81c
Manoj D U <dumanoj2003@gmail.com>
2024-03-16 09:29:04 +0530

    Initial commit

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start(); // Start the session
+
+// Check if the session is destroyed successfully
+if(session_destroy()) {
+    // Redirect the user to the home page
+    header("Location: /superstore/index.php");
+}
+?>
